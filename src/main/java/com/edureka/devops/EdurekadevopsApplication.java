adcb78d99e6c4be756c914641ac05e8d1844b420
vilasvarghese <vilas.varghese@gmail.com>
2020-09-09 10:29:10 +0530

    Adding devops project

---
+++ src/main/java/com/edureka/devops/EdurekadevopsApplication.java
@@ -0,0 +1,13 @@
+package com.edureka.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdurekadevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdurekadevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edureka/devops/EdurekadevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edureka.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdurekadevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
